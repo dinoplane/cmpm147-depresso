12d3983b5ed5c1523384e199e9237a8995369505
TeiHek <lerandy1235@gmail.com>
2022-07-12 12:44:46 -0700

    Added (currently untested) maze generation alg

--- mazegen.js
+++ mazegen.js
@@ -26,6 +26,10 @@ While the stack is not empty
 class Maze{
     constructor(length){
         let size = length * length;
+        // Members added for use in generateMaze
+        this.cellCount = size;
+        this.cellLen = length;
+        this.cellWid = length;
         this.cells = Array.from(Array(size), _ => Array(size).fill(0));
         
     }
@@ -34,7 +38,70 @@ class Maze{
         this.cells[cell1][cell2] = this.cells[cell2][cell1] = 1;
     }
 
-    generateMaze(){
-        
+    /*
+        Written by Randy Le
+        Generates a maze using data provided in constructor
+    */
+    generateMaze() {
+        // For simplification of DFS movement
+        function getCoords(node) {
+            return { x: node % this.cellLen, y: Math.floor(node / this.cellLen) }
+        }
+        function getNode(x, y){
+            return y * this.cellLen + x;
+        }
+        // Determine how/when to set a seed
+        // Set random start location
+
+        let startNode = Math.floor(random() * this.cellCount );
+
+        // Create array of explored cells
+        let visited = [];
+        visited.push(startNode);
+        // Create "stack" for exploration
+        let stack = []
+        stack.push(startNode);
+
+        // DFS algorithm
+        // Check that the stack is not empty
+        while (stack.length) {
+            // Pop a cell from the current stack, make it current
+            let current = stack.pop();
+            // Get coordinates of current node
+            let currentCoords = getCoords(current);
+
+            // Check unvisited neighbors:
+            let neighbours = [];
+            // Left neighbor
+            if(x - 1 >= 0                && !visited.includes(getNode(currentCoords.x - 1, currentCoords.y    )))
+                neighbours.push(getNode(currentCoords.x - 1, currentCoords.y));
+            // Right neighbor
+            if(x + 1 <= this.cellLen - 1 && !visited.includes(getNode(currentCoords.x + 1, currentCoords.y    )))
+                neighbours.push(getNode(currentCoords.x - 1, currentCoords.y));
+            // Up neighbor
+            if(y - 1 >= 0                && !visited.includes(getNode(currentCoords.x    , currentCoords.y - 1)))
+                neighbours.push(getNode(currentCoords.x - 1, currentCoords.y));
+            // Down neighbor
+            if(y + 1 <= this.cellWid - 1 && !visited.includes(getNode(currentCoords.x    , currentCoords.y + 1)))
+                neighbours.push(getNode(currentCoords.x - 1, currentCoords.y));
+
+            if(neighbours.length) {
+                // If unvisited neighbors exist:
+                // Push current onto stack
+                stack.push(current);
+
+                // Choose a random unvisited neighbor
+                let neighbor = neighbours[Math.floor(random() * neighbours.length)];
+
+                // Remove wall between current and chosen cell
+                this.removeWall(current, neighbor);
+
+                // Mark the current wall as visited and push to stack
+                visited.push(neighbor);
+                stack.push(neighbor);
+            }
+        }
+
+
     }
 }
