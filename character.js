b7669ea7aa479a5df6ceb75a63cff848cd9be9bf
Jane Tran <tranjtrang@gmail.com>
2022-07-12 20:01:45 -0700

    character movement

--- character.js
+++ character.js
@@ -2,18 +2,23 @@ var x = 200;
 var y = 200;
 
 function draw(){
-    circle(x, y, 100);
-    
+    circle(x, y, 10);
+    fill("blue");
+
+    movement();
+}
+
+function movement(){
     if(keyIsPressed){
         if(keyCode == LEFT_ARROW){
             x--;
         } else if(keyCode == RIGHT_ARROW){
             x++;
-        } 
+        }
         if(keyCode == UP_ARROW){
-            y--;
+                y--;
         } else if(keyCode == DOWN_ARROW){
-            y++;
+                y++;
         }
     }
 }
