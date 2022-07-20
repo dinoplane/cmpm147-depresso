bb1325f6da03040f3880fc019782443bc96dd7bd
Jane Tran <tranjtrang@gmail.com>
2022-07-19 23:32:22 -0700

    student clothes

--- uniform.js
+++ uniform.js
@@ -1,7 +1,30 @@
-const PANTS = [];
+const SKIRT =
+[
+  {type: "v", x: 78, y: 140},
+  {type: "v", x: 84, y: 116},
+  {type: "v", x: 116, y: 116},
+  {type: "v", x: 122, y: 140},
+  {type: "v", x: 100, y: 140},
+];
 
-const SHIRT = 
-  
+const LEFT_SHOES =
+[
+  {type: "v", x: 96, y: 152},
+  {type: "b", x: 96, y: 162},
+  {type: "b", x: 88, y: 160},
+  {type: "b", x: 88, y: 152},
+];
+
+const RIGHT_SHOES =
+[
+  {type: "v", x: 112, y: 152},
+  {type: "b", x: 112, y: 162},
+  {type: "b", x: 104, y: 160},
+  {type: "b", x: 104, y: 152},
+];
+
+
+/*const SHIRT =
   [
     {type: "v", x: 126, y: 90},
     {type: "v", x: 122, y: 104},
@@ -15,10 +38,39 @@ const SHIRT =
     {type: "b", x: 96, y: 88},
     {type: "b", x: 104, y: 88},
     {type: "b", x: 108, y: 82},
+  ];*/
+const SHIRT =
+  [
+    {type: "v", x: 112, y: 96},
+    {type: "v", x: 118, y: 100},
+    {type: "v", x: 124, y: 90},
+    {type: "v", x: 108, y: 84},
+    {type: "v", x: 100, y: 92},
+    {type: "v", x: 92, y: 84},
+    {type: "v", x: 78, y: 92},
+    {type: "v", x: 84, y: 100},
+    {type: "v", x: 88, y: 98},
+    {type: "b", x: 78, y: 118},
+    {type: "b", x: 84, y: 120},
+    {type: "b", x: 118, y: 118},
   ];
-  
 
-
+const TIE =
+[
+  {type: "v", x: 100, y: 90},
+  {type: "b", x: 94, y: 96},
+  {type: "b", x: 88, y: 88},
+  {type: "b", x: 92, y: 84},
+  {type: "b", x: 106, y: 98},
+  {type: "b", x: 110, y: 92},
+  {type: "b", x: 108, y: 84},
+  {type: "b", x: 92, y: 94},
+  {type: "b", x: 86, y: 114},
+  {type: "b", x: 100, y: 94},
+  {type: "b", x: 106, y: 104},
+  {type: "b", x: 110, y: 104},
+  {type: "b", x: 106, y: 98},
+];
 
 const a =  [
     {type: "v", x: 122, y: 142},
@@ -26,24 +78,24 @@ const a =  [
     {type: "v", x: 88, y: 84},
     {type: "v", x: 80, y: 142},
   ];
-  
-  const CLOTHES = {"something1": SHIRT, "something2": SHIRT, "something3": SHIRT};
+
+  const CLOTHES = {"something1": SHIRT, "something2":  SKIRT, "something3": TIE};
 
 // class StudentRender{
 //     renderHat(){
 
 //     }
-    
+
 //     renderShirt(){
 
 //     }
-    
+
 //     renderPants(){
-    
+
 //     }
-    
+
 //     renderSkirt(){
-    
+
 //     }
 
 
@@ -51,12 +103,12 @@ const a =  [
 //       push();
 //       noStroke();
 //       fill(color);
-      
-      
+
+
 //       beginShape();
 //       for (let i = 0; i < v.length; i++){
 //         //console.log(i, v[i].type)
-  
+
 //         if (v[i].type == "v")
 //           vertex(v[i].x, v[i].y);
 //         if (v[i].type == "c")
@@ -69,25 +121,21 @@ const a =  [
 //               break;
 //             bezierCache.push(v[i].x, v[i].y)
 //           }
-          
+
 //           if (bezierCache.length == 6){
-//             bezierVertex(...bezierCache);        
+//             bezierVertex(...bezierCache);
 //           }
 //           i -= 1;
-          
+
 //           //console.log(bezierCache)
 //         }
-          
+
 //       }
-      
-      
+
+
 //       endShape(CLOSE);
 //       pop();
-      
+
 //     }
-    
+
 // }
-
-
-
-
